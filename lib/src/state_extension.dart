d4a63ee84f98b061cdeeea0e21cf5a1b32a4db31
TheUltimateOptimist <digitalConfidence@web.de>
2023-02-16 20:29:48 +0100

    small refactoring state_extension #4

--- lib/src/state_extension.dart
+++ lib/src/state_extension.dart
@@ -3,8 +3,12 @@ import 'package:flutter/material.dart';
 import 'provided_state.dart';
 
 extension StateExtension on BuildContext {
-  ProvidedState _getProvidedState<T>() {
-    final providedState = InheritedModel.inheritFrom<ProvidedState<ShouldRebuildCallback<T>>>(this, aspect: (Object? _, Object? __) => true);
+  bool Function(Object?, Object?) get _alwaysRebuild{
+    return (Object? _, Object? __) => true;
+  }
+
+  ProvidedState _getProvidedState<T>(bool Function(Object?, Object?) aspect) {
+    final providedState = InheritedModel.inheritFrom<ProvidedState<ShouldRebuildCallback<T>>>(this, aspect: aspect);
     if (providedState == null) {
       throw Exception(
           "No StateProvider<$T> could be found in the current BuildContext. Please make sure to wrap the section of the widget tree in which you want to use it with StateProvider<$T>.");
@@ -12,19 +16,18 @@ extension StateExtension on BuildContext {
     return providedState;
   }
 
-  T state<T>() => _getProvidedState<T>().state as T;
+  T state<T>() => _getProvidedState<T>(_alwaysRebuild).state as T;
 
-  bool isLoading<T>() => _getProvidedState<T>().isLoading;
-  
+  bool isLoading<T>() => _getProvidedState<T>(_alwaysRebuild).isLoading;
 
   void collect<T>(T Function(T state) stateMapper) {
-    final appState = _getProvidedState<T>();
+    final appState = _getProvidedState<T>(_alwaysRebuild);
     final newState = stateMapper(appState.state as T);
     appState.collect(newState);
   }
 
   Future<void> collectFuture<T>(Future<T> Function(T state) stateMapper) async{
-    final providedState = _getProvidedState<T>();
+    final providedState = _getProvidedState<T>(_alwaysRebuild);
     providedState.startLoading();
     final newState = await stateMapper(providedState.state as T);
     providedState.collect(newState);
