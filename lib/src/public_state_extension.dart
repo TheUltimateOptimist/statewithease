06de558710740c145cd2ae93bb5b8ebe6c1827d0
TheUltimateOptimist <digitalConfidence@web.de>
2023-02-23 23:57:43 +0100

    fixed #15

--- lib/src/public_state_extension.dart
+++ lib/src/public_state_extension.dart
@@ -32,27 +32,53 @@ extension IsLoadingExtension on BuildContext{
   bool isLoading<T>() => getWrappedState<T>(rebuildAfterisLoadingChange).isLoading;
 }
 
-
-extension CollectExtension on BuildContext {
-  Future<void> collect<T>(
-    dynamic stateMapper, [
-    dynamic extra = const Ignore(),
-    dynamic secondExtra = const Ignore(),
-    dynamic thirdExtra = const Ignore(),
-  ]) async {
+extension CollectExtension on BuildContext{
+  void collect<T>(T Function(T) stateMapper){
     final stateModel = getStateModel<T>(neverRebuild);
-    final dynamic newState;
-    if (extra is Ignore) {
-      assert(stateMapper is T Function(T));
-      newState = stateMapper(stateModel.wrappedState.state);
-    } else if (secondExtra is Ignore) {
-      newState = stateMapper(stateModel.wrappedState.state, extra);
-    } else if (thirdExtra is Ignore) {
-      newState = stateMapper(stateModel.wrappedState.state, extra, secondExtra);
-    } else {
-      newState = stateMapper(stateModel.wrappedState.state, extra, secondExtra, thirdExtra);
-    }
-    assert(newState is T || newState is Future<T>);
-    stateModel.collect(await newState);
+    final newState = stateMapper(stateModel.wrappedState.state);
+    stateModel.collect(newState);
+  }
+}
+
+extension CollectOneExtension on BuildContext{
+  void collectOne<T, P1>(T Function(T, P1) stateMapper, P1 param1){
+    final stateModel = getStateModel<T>(neverRebuild);
+    final newState = stateMapper(stateModel.wrappedState.state, param1);
+    stateModel.collect(newState);
+  }
+}
+
+extension CollectTwoExtension on BuildContext{
+  void collectTwo<T, P1, P2>(T Function(T, P1, P2) stateMapper, P1 param1, P2 param2){
+    final stateModel = getStateModel<T>(neverRebuild);
+    final newState = stateMapper(stateModel.wrappedState.state, param1, param2);
+    stateModel.collect(newState);
+  }
+}
+
+extension CollectFutureExtension on BuildContext{
+  Future<void> collectFuture<T>(Future<T> Function(T) stateMapper) async{
+    final stateModel = getStateModel<T>(neverRebuild);
+    stateModel.startLoading();
+    final newState = await stateMapper(stateModel.wrappedState.state);
+    stateModel.collect(newState);
+  }
+}
+
+extension CollectFutureOneExtension on BuildContext{
+  Future<void> collectFutureOne<T, P1>(Future<T> Function(T, P1) stateMapper, P1 param1) async{
+    final stateModel = getStateModel<T>(neverRebuild);
+    stateModel.startLoading();
+    final newState = await stateMapper(stateModel.wrappedState.state, param1);
+    stateModel.collect(newState);
+  }
+}
+
+extension CollectFutureTwoExtension on BuildContext{
+  Future<void> collectFutureTwo<T, P1, P2>(Future<T> Function(T, P1, P2) stateMapper, P1 param1, P2 param2) async{
+    final stateModel = getStateModel<T>(neverRebuild);
+    stateModel.startLoading();
+    final newState = await stateMapper(stateModel.wrappedState.state, param1, param2);
+    stateModel.collect(newState);
   }
 }
