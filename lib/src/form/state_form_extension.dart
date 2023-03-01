8e6869588730c9d50fe5f9614733e0596f59f779
TheUltimateOptimist <digitalConfidence@web.de>
2023-03-01 12:18:44 +0100

    fixed #20

--- lib/src/form/state_form_extension.dart
+++ lib/src/form/state_form_extension.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
+import '../state_mapper_exceptions.dart';
 import 'custom_form_state.dart';
 import '../public_state_extensions.dart';
 
-extension SubmitForm on BuildContext{
-  void submitFormSync<T extends Enum>(CustomFormState<T> Function(CustomFormState<T>) stateMapper, {void Function(BuildContext)? callback}){
+extension SubmitForm on BuildContext {
+  void submitFormSync<T extends Enum>(String? Function(Map<T, String>) valuesMapper, {void Function(BuildContext)? callback}) {
     final state = read<CustomFormState<T>>();
-    if(state.validate()){
-      collect(stateMapper, callback: callback);
+    if (state.validate()) {
+      collect(
+        (CustomFormState<T> state) {
+          final errorMessage = valuesMapper(state.values);
+          if (errorMessage != null) {
+            return state.withSubmitErrorMessage(errorMessage);
+          }
+          throw IgnoreState();
+        },
+        callback: callback,
+      );
     }
   }
 
-  void submitFormAsync<T extends Enum>(Future<CustomFormState<T>> Function(CustomFormState<T>) stateMapper, {void Function(BuildContext)? callback}){
+  void submitFormAsync<T extends Enum>(Future<String?> Function(Map<T, String>) valuesMapper, {void Function(BuildContext)? callback}) {
     final state = read<CustomFormState<T>>();
-    if(state.validate()){
-      if(state.submitErrorMessage != null){
+    if (state.validate()) {
+      if (state.submitErrorMessage != null) {
         collect((CustomFormState<T> state) => state.withSubmitErrorMessage(null));
       }
-      collectFuture(stateMapper, callback: callback);
+      collectFuture(
+        (CustomFormState<T> state) async {
+          final errorMessage = await valuesMapper(state.values);
+          if (errorMessage != null) {
+            return state.withSubmitErrorMessage(errorMessage);
+          }
+          throw IgnoreState();
+        },
+        callback: callback,
+      );
     }
   }
 
